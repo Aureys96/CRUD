9379002f1b4233da2578fccc49974f94bc7921f1
Kostyan <konstantingolubev8@gmail.com>
2019-10-09 16:47:34 +0300

    Testing profiles

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 import Back.DataBase.DBService;
 import Back.DataBase.DBServiceInt;
-
+import Back.BL.Profile.Profile;
 public class Main {
     public static void main(String[] args) {
         DBServiceInt db = new DBService();
         db.printConnectInfo();
+        try {
+            long userId = db.addProfile("Aureys","1234","fox@gmail.com");
+            System.out.println("Added user id: " + userId);
+
+            Profile profile = db.getProfile(userId);
+            System.out.println("User profile: " + profile);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
